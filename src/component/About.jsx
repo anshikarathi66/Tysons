98bbea8594c34bd280272864333911bdab7f48dd
abhijeetmaan <96488059+abhijeetmaan@users.noreply.github.com>
2024-03-06 15:40:31 +0530

    Update About.jsx

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,82 +1,56 @@
-
-
 import React from 'react';
 import './CSS/about.css'; // Import your custom CSS file
+import chirag_tiwari from './images/Chirag_Tiwari.jpg';
 
+import './CSS/about.css';
 function About() {
-  return (
-    // <div className="about-container">
-    //   <div className="about-description">
-    //     <p className="about-heading">About Us</p>
-    //     <p className="about-welcome">Welcome To Tysons Legal Allies</p>
-    //     <p className="about-paragraph">
-    //       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, vel? Labore quod veniam magni quas minus ducimus, ex tempore asperiores debitis officiis tempora atque illo repellat architecto qui reiciendis perferendis ad, modi voluptatibus doloribus repellendus placeat dolor! Deleniti consequuntur praesentium iusto dolorum quas eos, suscipit odit. Dolorem omnis in, iusto at, dignissimos veritatis voluptate tempora aspernatur itaque blanditiis voluptas quae, cumque pariatur numquam adipisci? Quibusdam a porro corporis modi dolorem saepe veniam ea aperiam minima. Sit voluptatibus reprehenderit magnam architecto, labore ullam. Vitae nisi praesentium, cum sint optio officia beatae, labore aut iusto quis eius reiciendis minima quasi exercitationem alias? mattis ac neque.
-    //     </p>
-    //     <p className="about-heading">CEO & Founder of Tyson</p>
-    //   </div>
-    //   <div className="about-image">
-    //     <img
-    //       src="https://picsum.photos/400"
-    //       alt="CEO"
-    //       className="img-fluid"
-    //     />
-    //     <button className="know-more-button">Know More</button>
-    //   </div>
-    // </div>
 
+  return (
     <section className="about-section">
-  <div className="container">
-    <div className="row clearfix">
-      {/*Content Column*/}
-      <div className="content-column col-md-6 col-sm-12 col-xs-12">
-        <div className="inner-column">
-          <div className="sec-title">
-            <div className="title">About Us</div>
-            <h2>
-            Welcome To <br /> Tysons Legal Allies
-            </h2>
+      <div className="container">
+        <div className="row clearfix">
+          {/* Content Column */}
+          <div className="content-column col-md-6 col-sm-12 col-xs-12">
+            <div className="inner-column">
+              <div className="sec-title">
+                <div className="title">About Us</div>
+                <h2 className='about-h2'>
+                  Welcome To <br /> Tysons Legal Allies
+                </h2>
+              </div>
+              <div className="text">
+                Welcome to Tysons Legal Allies! Beyond a platform, we've created a vibrant community redefining legal education. With engaging events like Moot Courts and Placement Drives, we infuse personality into the legal journey. Join us for a unique, impactful, and enjoyable adventure in the legal realm. Cheers to a legal journey like no other!
+              </div>
+              <div className="email">
+                Founder & CEO of Tysons
+              </div>
+              <a href="about.html" className="theme-btn btn-style-three">
+                Read More
+              </a>
+            </div>
           </div>
-          <div className="text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries
-          </div>
-          <div className="email">
-            Request Quote:{" "}
-            <span className="theme_color">freequote@gmail.com</span>
-          </div>
-          <a href="about.html" className="theme-btn btn-style-three">
-            Read More
-          </a>
-        </div>
-      </div>
-      {/*Image Column*/}
-      <div className="image-column col-md-6 col-sm-12 col-xs-12">
-        <div
-          className="inner-column "
-          data-wow-delay="0ms"
-          data-wow-duration="1500ms"
-        >
-          <div className="image">
-            <img src="https://i.ibb.co/vQbkKj7/about.jpg" alt="" />
-            <div className="overlay-box">
-              <div className="year-box">
-                <span className="number">5</span>Years <br /> Experience <br />{" "}
-                Working
+          {/* Image Column */}
+          <div className="image-column col-md-6 col-sm-12 col-xs-12">
+            <div
+              className="inner-column "
+              data-wow-delay="0ms"
+              data-wow-duration="1500ms"
+            >
+              <div className="image">
+                {/* Lazy load the image */}
+                <img src={chirag_tiwari} alt="" loading="lazy" />
+                <div className="overlay-box">
+                  <div className="year-box">
+                    <span className="number">Chirag K. Tiwari</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
-
+    </section>
   );
 }
 
 export default About;
-
-
