a898749789c0a7fa15650e22c76c1945bb5fd8af
abhijeetmaan <96488059+abhijeetmaan@users.noreply.github.com>
2024-03-06 15:48:09 +0530

    Update Team.jsx

--- src/component/Team.jsx
+++ src/component/Team.jsx
@@ -1,24 +1,74 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import Slider from 'react-slick';
 import './CSS/team.css';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import chirag_tiwari1 from './images/Chirag_Tiwari1.jpg'
+import vanshika_saini1 from './images/vanshika_saini1.jpg'
+import aryaman_singh1 from './images/aryaman_singh1.jpg'
+import rithik_shankar1 from './images/rithik_shankar1.jpg'
+import aditya_bachkoti1 from './images/aditya_bachkoti1.jpg'
+import anshika_rathi from './images/anshika_rathi.jpg'
+import Swiper from 'swiper';
 
 function Team() {
+  const [centerCardIndex, setCenterCardIndex] = useState(0);
+  const teamMembers = [
+    {
+      imageSrc: chirag_tiwari1,
+      name: 'Chirag K. Tiwari',
+      role: 'Founder & CEO',
+      quote: 'Tysons Legal Allies, led by Chirag K. Tiwari, streamlines law aspirants journeys with tailored event solutions, paving a seamless path to success.',
+    },
+    {
+      imageSrc: vanshika_saini1,
+      name: 'Vanshika Saini',
+      role: 'Ambassador',
+      quote: 'Ambassador Vanshika Saini, a dynamic force at Tysons Legal Allies, passionately champions tailored event solutions for success in the legal realm. Join her for an exceptional legal journey.',
+    },
+    {
+      imageSrc: aryaman_singh1,
+      name: 'Aryaman Singh',
+      role: 'Operational Head',
+      quote: 'As Operational Head, Aryaman Singh leads Tysons Legal Allies with precision, ensuring flawless execution of tailored legal events. Trust him for operational excellence in our dynamic community.',
+    },
+    {
+      imageSrc: rithik_shankar1,
+      name: 'Rithik Shankar',
+      role: 'Operational Head',
+      quote: 'Operational Head Rithik Shankar ensures flawless execution at Tysons Legal Allies, embodying excellence in leadership and legal event management. ',
+    },
+    {
+      imageSrc: aditya_bachkoti1,
+      name: 'Aditya Bachkoti',
+      role: 'Project Manager',
+      quote: 'Aditya Bachkoti, Tysons Legal Allies Project Manager, leads with strategic vision, ensuring successful project implementation for excellence in our dynamic legal community. ',
+    },
+    {
+      imageSrc: anshika_rathi,
+      name: 'Anshika Rathi',
+      role: 'IT Cell Head',
+      quote: 'Anshika Rathi, Tysons Legal Allies IT Cell Head, leads technological innovation for a seamless digital experience, contributing to excellence in our dynamic community.',
+    },
+    // ... Add more team members here
+  ];
+  // Your slider settings 
   const sliderSettings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0',
     responsive: [
       {
-        breakpoint: 680,
+        breakpoint: 767,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
-          infinite: false,
+          centerMode: false,
         },
       },
       {
@@ -26,95 +76,34 @@ function Team() {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          nav: true,
+          centerMode: true,
         },
       },
     ],
+    beforeChange: (current, next) => setCenterCardIndex(next),
   };
 
   return (
     <div className="gtco-testimonials">
-      <h2>Testimonials Carousel - Cards Comments</h2>
+      <p className='Meet'>Meet Our Legal Allies</p>
+      <p className='Professions'>Connecting Professions for Shared Success</p>
       <Slider {...sliderSettings}>
-        <div>
-          <div className="card text-center">
-            <img
-              className="card-img-top"
-              src="https://images.unsplash.com/photo-1572561300743-2dd367ed0c9a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300"
-              alt=""
-            />
-            <div className="card-body">
-              <h5>
-                Ronne Galle <br />
-                <span> Project Manager </span>
-              </h5>
-              <p className="card-text">
-                “ Nam libero tempore, cum soluta nobis est eligendi optio cumque
-                nihil impedit quo minus id quod maxime placeat ”{" "}
-              </p>
+        {teamMembers.map((member, index) => (
+          <div key={index}>
+            <div
+              className={`card text-center ${centerCardIndex === index ? 'focused' : 'blurred'}`}
+            >
+              <img className="card-img-top" src={member.imageSrc} alt={`Team Member - ${index}`} />
+              <div className="card-body">
+                <h5 className='team-h5'>
+                  {member.name} <br />
+                  <span> {member.role} </span>
+                </h5>
+                <p className="card-text">{member.quote}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div className="card text-center">
-            <img
-              className="card-img-top"
-              src="https://images.unsplash.com/photo-1588361035994-295e21daa761?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=301&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=301"
-              alt=""
-            />
-            <div className="card-body">
-              <h5>
-                Missy Limana
-                <br />
-                <span> Engineer </span>
-              </h5>
-              <p className="card-text">
-                “ Nam libero tempore, cum soluta nobis est eligendi optio cumque
-                nihil impedit quo minus id quod maxime placeat ”{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="card text-center">
-            <img
-              className="card-img-top"
-              src="https://images.unsplash.com/photo-1575377222312-dd1a63a51638?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=302&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=302"
-              alt=""
-            />
-            <div className="card-body">
-              <h5>
-                Martha Brown
-                <br />
-                <span> Project Manager </span>
-              </h5>
-              <p className="card-text">
-                “ Nam libero tempore, cum soluta nobis est eligendi optio cumque
-                nihil impedit quo minus id quod maxime placeat ”{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="card text-center">
-            <img
-              className="card-img-top"
-              src="https://images.unsplash.com/photo-1549836938-d278c5d46d20?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=303&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=303"
-              alt=""
-            />
-            <div className="card-body">
-              <h5>
-                Hanna Lisem
-                <br />
-                <span> Project Manager </span>
-              </h5>
-              <p className="card-text">
-                “ Nam libero tempore, cum soluta nobis est eligendi optio cumque
-                nihil impedit quo minus id quod maxime placeat ”{" "}
-              </p>
-            </div>
-          </div>
-        </div>
+        ))}
       </Slider>
     </div>
   );
