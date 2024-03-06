16d111040d94b868e063e5aafa732d7eb5e0f87b
abhijeetmaan <96488059+abhijeetmaan@users.noreply.github.com>
2024-03-06 15:47:19 +0530

    Update Services.jsx

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,7 +1,5 @@
-// Services.js
-
 import React from 'react';
-import './CSS/services.css'; // Make sure to import the CSS file
+import './CSS/services.css';
 import ClientConsulation from "./images/client_consultation.png";
 import Mentor from "./images/mentor.png";
 import EventManagement from "./images/event-management.png";
@@ -21,8 +19,9 @@ function Services() {
           <div className="service-box">
             <div className="service-content">
               <h4 className='service-h4'>CLIENT CONSULTATION</h4>
-              <img src={ClientConsulation} alt="client consultation" className="service-image" />
-              <p className='service-caption'>Description for Service 1 Lorem ipsum dolor sit amet consectetur.</p>
+              <img src={ClientConsulation} alt="client consultation" className="service-image" loading="lazy" />
+
+              <p className='service-caption'>Your legal journey starts with insightful client consultations for success.</p>
             </div>
           </div>
         </div>
@@ -32,8 +31,8 @@ function Services() {
           <div className="service-box">
             <div className="service-content">
               <h4 className='service-h4'>MENTORING</h4>
-              <img src={Mentor} alt="mentoring" className="service-image" />
-              <p className='service-caption'>Description for Service 2 Lorem ipsum dolor sit amet consectetur.</p>
+              <img src={Mentor} alt="mentoring" className="service-image" loading='lazy' />
+              <p className='service-caption'>Empowering Legal Minds: Mentorship for Your Success Journey.</p>
             </div>
           </div>
         </div>
@@ -43,8 +42,8 @@ function Services() {
           <div className="service-box">
             <div className="service-content">
               <h4 className='service-h4'>EVENT MANAGEMENT</h4>
-              <img src={EventManagement} alt="event management" className="service-image" />
-              <p className='service-caption'>Description for Service 3 Lorem ipsum dolor sit amet consectetur.</p>
+              <img src={EventManagement} alt="event management" className="service-image" loading='lazy' />
+              <p className='service-caption'>Crafting Legal Experiences: Events Tailored for Excellence and Engagement.</p>
             </div>
           </div>
         </div>
@@ -56,8 +55,8 @@ function Services() {
           <div className="service-box">
             <div className="service-content">
               <h4 className='service-h4'>ASSISTANCE</h4>
-              <img src={Assistance} alt="assistance" className="service-image" />
-              <p className='service-caption'>Description for Service 4 Lorem ipsum dolor sit amet consectetur.</p>
+              <img src={Assistance} alt="assistance" className="service-image" loading='lazy' />
+              <p className='service-caption'>Guiding Your Legal Path: Comprehensive Assistance Tailored for Your Success.</p>
             </div>
           </div>
         </div>
@@ -67,8 +66,8 @@ function Services() {
           <div className="service-box">
             <div className="service-content">
               <h4 className='service-h4'>CERTIFICATIONS</h4>
-              <img src={Certificate} alt="certificate" className="service-image" />
-              <p className='service-caption'>Description for Service 5 Lorem ipsum dolor sit amet consectetur.</p>
+              <img src={Certificate} alt="certificate" className="service-image" loading='lazy' />
+              <p className='service-caption'>Unlock Your Potential: Certifications Tailored for Legal Excellence and Growth.</p>
             </div>
           </div>
         </div>
